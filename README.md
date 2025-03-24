5d8e4caf30549d1fd80dc301a4d848353c907f06
rabbyxq <rabbyxq@gmail.com>
2025-03-25 00:23:16 +0600

    Update

---
+++ README.md
@@ -0,0 +1,9 @@
+LATEX RECOGNIZER FOR NEWTONIX AI
+
+DATASET
+
+
+data = kagglehub.dataset_download("gregoryeritsyan/im2latex-230k")
+
+
+
---
+++ data.py
@@ -0,0 +1,8 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("gregoryeritsyan/im2latex-230k")
+
+print("Path to dataset files:", path)
+
+
